cc1e4428ca8bf3a7184a434933c76a98d3b26149
Danilo C. Graciano <danilocgraciano@gmail.com>
2019-05-16 09:13:37 -0300

    Instalação de módulo para aumentar a segurança da aplicação.

--- web/config/express.js
+++ web/config/express.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+app.use(helmet());
 
 app.use('/node_modules', express.static('./node_modules'));
 app.use('/js', express.static('./js'));
